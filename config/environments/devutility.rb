5e8354a37c42362b3a10257c72f262c3ac6869c6
Atul Patel <Atul_Patel@external.mckinsey.com>
2013-02-27 03:34:24 -0800

    changing devutility.rb for assets

--- config/environments/devutility.rb
+++ config/environments/devutility.rb
@@ -28,6 +28,7 @@ Worldmap::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/ 
 
   # Do not compress assets
   config.assets.compress = false
