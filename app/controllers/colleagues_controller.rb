729e2e17ddf936ef78a734bc3e3b50c80d4e8122
Nikita Singh <nikitaa_singh@yahoo.co.in>
2013-03-05 05:33:45 -0800

    image in info window

--- app/controllers/colleagues_controller.rb
+++ app/controllers/colleagues_controller.rb
@@ -132,7 +132,7 @@ class ColleaguesController < ApplicationController
 
   @location= @location+ @start+ 
   @place + '"'+colleague.location+'",'+
-  @image+'"public'+colleague.attachment.url(:medium)+'"' +
+  @image+'"worldmap'+colleague.attachment.url(:medium)+'"' +
  
   @end+","
 
