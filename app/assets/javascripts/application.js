30660981b59124440a954dd600e871b35146e389
Nikita Singh <nikitaa_singh@yahoo.co.in>
2013-03-14 03:37:15 -0700

    Issue solved for datatable and latitude longitude

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,27 +18,3 @@ $(document).ready(function(){
     $('#index_table').dataTable();
 });
 
-
-$(function(){
-    if($("#devise_notice").text() != ""){
-        createDialog("<h3 class='heading3 alert-message'>"+ $("#devise_notice").text() +"</h3>");
-        $("#devise_notice").remove();
-    }
-    if($("#devise_alert").text() != ""){
-        createDialog("<h3 class='heading3 alert-message'>"+ $("#devise_alert").text() +"</h3>");
-        $("#devise_alert").remove();
-    }
-    function createDialog( message) {
-        $.colorbox({
-            html : message,
-            transition: 'fade',
-            width: 600,
-            height: 130,
-            scrolling: false
-        });
-        setTimeout(function() {
-            $.colorbox.close();
-        }, 2000);
-    }
-
-});
